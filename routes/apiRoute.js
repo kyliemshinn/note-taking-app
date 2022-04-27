72d9d63084d8b55cd8eec5fdac53d4fc0e6671b5
Kylie Shinn <kyliemshinn@gmail.com>
2022-04-26 19:39:52 -0700

    add route variables and export data to routes folder files

---
+++ routes/apiRoute.js
@@ -0,0 +1,6 @@
+//variables required
+const apiRouter = require('express').Router();
+const data = require('../db/db.json');
+
+
+module.exports = apiRouter;
---
+++ routes/htmlRoute.js
@@ -0,0 +1,9 @@
+//variables required
+const apiRouter = require('express').Router();
+const data = require('../db/db.json');
+
+//set get request
+
+//set post request
+
+module.exports = apiRouter;
