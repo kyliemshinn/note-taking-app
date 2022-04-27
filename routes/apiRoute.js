7db313e137b2f18235d70efd9c9c550cb61ad33e
Kylie Shinn <kyliemshinn@gmail.com>
2022-04-27 10:57:37 -0700

    add heroku port

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -4,7 +4,11 @@ const data = require('../db/db.json');
 const path = require('path');
 
 //set get request
-apiRouter.get()
+apiRouter.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname + "/db/db.json") () => {
+        res.json(JSON.parse(object))
+    }
+})
 
 //set post request
 apiRouter.post()
