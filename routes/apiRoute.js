548dae8e456088717c2462c0b95148e0c0ef8cd0
Kylie Shinn <kyliemshinn@gmail.com>
2022-04-28 20:38:38 -0700

    get data to apend to db.json file

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,65 +1,87 @@
 //variables required
 const apiRouter = require("express").Router();
 const newData = require("../db/db.json");
-const path = require("path");
+//const path = require("path");
 const fs = require("fs");
-var uniqid = require("uniqid");
+// var uuidv1 = require("uuid/v1");
 
-
-  //set get request
-  apiRouter.get("/notes", (req, res) => {
-    res.json(newData);
-  });
-
-  //set post request
-  apiRouter.post("/notes", (req, res) => {
-    res.send("send some data");
-    //create new object this is equal to the body
-    const { title, text } = req.body;
-    //new note body
-    const newNote = {
-      title,
-      text,
-      id: uniqid(),
-    };
-
-    newData.push(newNote);
-    updateNotes();
-    
-    const updateNotes = (newNote, newData) => {
-        fs.readFile(newData, 'utf8', (err, newNote) => {
-            if (err) {
-                console.error(err);
-            } else {
-                // const parsedData = JSON.parse(data);
-                // parsedData.push(newNote);
-
-                fs.writeFileSync('../db/db/json', JSON.stringify(newNote));
-            }
-        });
-        
-        const response = {
-            status: "success",
-            body: newData,
-        };
-        
-        res.json(response)
-        
-    };
-
-//     const readFromFile = util.promisify(fs.readFile);
-// /**
-//  *  Function to write data to the JSON file given a destination and some content
-//  *  @param {string} destination The file you want to write to.
-//  *  @param {object} content The content you want to write to the file.
-//  *  @returns {void} Nothing
-//  */
-// const writeToFile = (destination, content) =>
-//   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-//     err ? console.error(err) : console.info(`\nData written to ${destination}`)
-//   );
-
-  readFromFile();
+//set get request
+apiRouter.get("/notes", (req, res) => {
+  res.json(newData);
 });
 
+//set post request
+apiRouter.post("/notes", (req, res) => {
+  //create new object this is equal to the body
+  const { title, text } = req.body;
+  //new note body
+  const newNote = {
+    title,
+    text,
+    // id: uuidv1(),
+  };
+
+  newData.push(newNote);
+
+  // fs.readFile(newData, 'utf8', (err, data) => {
+  //   if (err) {
+  //       console.error(err);
+  //     }
+  //   });
+
+  // const dataStr = JSON.stringify(newNote);
+
+  // fs.writeFile(newData, dataStr, (err) =>
+  //   err
+  //     ? console.error(err)
+  //     : console.log(
+  //         `Review for ${newNote.product} has been written to JSON file`
+  //       )
+  // );
+  
+// Gives back the response, which is the user's new note. 
+// res.json(newNote);
+
+
+
+  const response = {
+    status: "success",
+    body: newNote,
+  };
+  res.json(response)
+
+  // const updateNotes = (newNote, newData) => {
+ 
+        //   } else {
+        //       const parsedData = JSON.parse(data);
+        //       parsedData.push(newNote);
+        //       fs.writeFileSync('../db/db/json', parsedData);
+       
+    //   const response = {
+    //       status: "success",
+    //       body: newData, //should be new note?
+      // };
+
+    updateNotes();
+
+//   };
+
+});
+
+function updateNotes() {
+  fs.writeFile("db/db.json", JSON.stringify(newData), function (err) {
+      if (err) {
+          console.log("error")
+          return console.log(err);
+      }
+
+      console.log("Success!");
+  });
+}
+
+//delete addd :id
+
 module.exports = apiRouter;
+
+//add file to send a recieve
+// when it comes  back into the syustem needs to be stringified
--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -5,9 +5,14 @@ const data = require('../db/db.json');
 
 
 //set GET requests 
+// apiRouter.get("*", (req, res) => {
+//     res.send(path.join(__dirname + "../public/index.html"))
+//  });
+
 apiRouter.get("/notes", function(req, res){
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
+
 
 
 module.exports = apiRouter;
